74ddbc9b0cce359c9f5284b6018ccc66bdb3a762
nhkd <nhkd@netcompany.com>
2024-12-23 11:02:23 +0000

    feat(pages): split views

--- views/home.py
+++ views/home.py
@@ -1,3 +1,4 @@
+from streamlit_extras.button_selector import button_selector 
 import streamlit as st
 
 st.markdown(
@@ -10,3 +11,25 @@ st.markdown(
     - 👾 Github: ([rqkun](https://github.com/rqkun))
 """
 )
+
+def example():
+    month_list = [
+        "Add",
+        "View",
+        "Edit"
+    ]
+    selected_index = button_selector(
+        month_list,
+        index=0,
+        spec=3,
+        key="button_selector_example_month_selector",
+        label="Month Selector",
+    )
+    #st.write(f"Selected month: {month_list[selected_index]}")
+    if month_list[selected_index] == "Add":
+        st.switch_page("views/add.py")
+    if month_list[selected_index] == "View":
+        st.switch_page("views/view.py")
+    if month_list[selected_index] == "Edit":
+        st.switch_page("views/manage.py")
+example()
---
+++ views/view.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from datetime import datetime
+import lib.common as common
+
+conn,worksheet_names = common.get_sheets()
+
+
+if 'sheet_key' not in st.session_state:
+    st.session_state['sheet_key'] =datetime.today().strftime('%B-%Y')
+
+if 'sheet' not in st.session_state:
+    st.session_state['sheet'] = common.clean(conn.read(worksheet=st.session_state['sheet_key']))
+    
+
+col1,col2 = st.columns([5,2])
+
+sheet = st.session_state['sheet']
+
+
+
+with col1:
+    option = st.selectbox(label="Sheet Select",
+                        options = worksheet_names,
+                        index=common.find_key(worksheet_names,st.session_state['sheet_key']),
+                        label_visibility="collapsed"
+                    )
+    if option:
+        sheet = common.clean(conn.read(worksheet=option))
+        st.session_state['sheet'] = sheet
+    
+
+with col2:
+    refresh_button = st.button("Refresh",use_container_width=True, icon=":material/sync:",type="primary")
+    
+
+
+st.dataframe(sheet,use_container_width=True,height=35*len(sheet)+38,hide_index=True)
+
+if refresh_button: 
+    st.cache_data.clear()
+    st.cache_resource.clear()
+    st.rerun()
