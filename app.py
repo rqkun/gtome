ab3dbb9fe1f1e854c731a4c2959bf37a58a57d62
nhkd <nhkd@netcompany.com>
2024-12-24 03:00:51 +0000

    feat(authentication): added login. logout base on session

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import lib.authentication as auth
 
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+def login():
+    auth.login()
+    
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 home_page = st.Page("views/home.py", title="Home", icon=":material/home:")
 connection_page = st.Page("views/add.py", title="Add", icon=":material/add_circle:")
 edit_page = st.Page("views/manage.py", title="Manage", icon=":material/edit:")
 view_page = st.Page("views/view.py", title="View", icon=":material/search:")
 
-pg = st.navigation([home_page, connection_page,edit_page,view_page],position="hidden")
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+login_pages = [home_page, connection_page,edit_page,view_page]
+
+if st.session_state.login == True:
+    pg = st.navigation(login_pages,position="hidden")
+else:
+    pg = st.navigation([st.Page(login)],position="hidden")
+
 pg.run()
---
+++ lib/authentication.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def login():
+
+    st.header("Authentication")
+    account = st.text_input("Username")
+    
+    
+    col1, col2 = st.columns([5,1],vertical_alignment="bottom")
+    with col1:
+        password = st.text_input("Password",type="password")
+    with col2:
+        login_bttn = st.button("Login",use_container_width=True,type="primary",icon=":material/key:")
+    if login_bttn:
+        if (st.secrets.account.admin == account and st.secrets.account.password == password):
+            st.session_state.login = True
+            st.rerun()
+        else:
+            st.error("Wrong account or password.",icon=":material/sentiment_stressed:")
+    
+        
+def logout():
+    st.session_state.login = False
+    st.rerun()
