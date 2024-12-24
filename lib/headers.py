b79fffcbdb5b54024d9c975b4eaa625b7e26a191
nhkd <nhkd@netcompany.com>
2024-12-24 03:00:51 +0000

    feat(authentication): added login. logout base on session

--- lib/headers.py
+++ lib/headers.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+import lib.authentication as auth
 def add_header():
     with st.header(""):
         left, _, right = st.columns([1,4,1])
         if left.button("Menu","", use_container_width=True,type="secondary", icon=":material/menu_open:"):
             st.switch_page("views/home.py")
         if right.button("Logout","", use_container_width=True,type="secondary", icon=":material/exit_to_app:"):
-            st.switch_page("views/home.py")
+            auth.logout()
